a495f613914256bb0b69c9c83b87a63c730d9a65
liaoxin <liaoxin@meizu.com>
2018-07-29 15:16:42 +0800

    初始化demo工程
    初始化demo工程

---
+++ temp/quick-scripts/assets/js/Game.js
@@ -0,0 +1,117 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/js/Game.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '2986cezrR9Ec5ZRG/fii1q9', 'Game', __filename);
+// js/Game.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // 这个属性引用了星星预制资源
+        starPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        // 星星产生后消失时间的随机范围
+        maxStarDuration: 0,
+        minStarDuration: 0,
+        // 地面节点，用于确定星星生成的高度
+        ground: {
+            default: null,
+            type: cc.Node
+        },
+        // player 节点，用于获取主角弹跳的高度，和控制主角行动开关
+        player: {
+            default: null,
+            type: cc.Node
+        },
+        pindex: 0
+    },
+    onLoad: function onLoad() {
+        // 获取地平面的 y 轴坐标
+        this.groundY = this.ground.y + this.ground.height / 2;
+        // 生成一个新的星星
+        this.spawnNewStar();
+        this.setInputControl();
+    },
+
+    spawnNewStar: function spawnNewStar() {
+
+        // 使用给定的模板在场景中生成一个新节点
+        var newStar = cc.instantiate(this.starPrefab);
+        // 将 Game 组件的实例传入星星组件
+        newStar.getComponent('Star').game = this;
+        // 将新增的节点添加到 Canvas 节点下面
+        this.node.addChild(newStar);
+        // 为星星设置一个随机位置
+        newStar.setPosition(this.getNewStarPosition());
+    },
+
+    getNewStarPosition: function getNewStarPosition() {
+        var self = this;
+        var randX = 0;
+        // 根据地平面位置和主角跳跃高度，随机得到一个星星的 y 坐标
+        var randY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight + 50;
+        // 根据屏幕宽度，随机得到一个星星 x 坐标
+        var maxX = this.node.width / 2;
+        randX = cc.randomMinus1To1() * maxX;
+        // 返回星星坐标
+        return cc.p(240 * self.pindex, -240);
+    },
+    setInputControl: function setInputControl() {
+        var self = this;
+        // 添加键盘事件监听
+        // 有按键按下时，判断是否是我们指定的方向控制键，并设置向对应方向加速
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function (event) {
+            console.log(self.pindex);
+            switch (event.keyCode) {
+                case cc.KEY.a:
+                    if (self.pindex <= -1) {
+                        break;
+                    }
+                    self.pindex -= 1;
+                    break;
+                case cc.KEY.d:
+                    if (self.pindex >= 1) {
+                        break;
+                    }
+                    self.pindex += 1;
+                    break;
+            }
+        });
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {}
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=Game.js.map
+        
